c21c4c987dcf2d8bfcb93768f894d2ef73b226f9
muiloco <muiloco17@gmail.com>
2019-10-03 18:08:48 -0300

    Inclusão de Exercicio 17

---
+++ atividades ADS/ListaDeExercicios/src/exercicio17.java
@@ -0,0 +1,17 @@
+
+public class exercicio17 {
+
+	public static void main(String[] args) {
+		
+		double graos = 1;
+		double soma = 0;
+		int casas = 64;
+		for (int i = 1; i <= casas; i++) {
+			soma += graos;
+			graos = graos * 2;
+		}
+		System.out.println("A quantidade de grãos é:"+ soma);
+		
+	}
+	
+}
